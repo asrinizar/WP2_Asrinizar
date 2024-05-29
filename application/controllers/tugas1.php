22517160628edb6e399a51907b262491b435e1a2
nijar8 <asrinizar8@gmail.com>
2024-05-29 17:17:57 +0700

    tugas1

--- application/controllers/tugas1.php
+++ application/controllers/tugas1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Reza Maulana,
+        echo"Nama saya Asri Nizar,
             Saya tinggal di Kota Pontianak,
             olah raga yang saya sukai adalah
-            Running";
+            Menonton, saya mahasiswa universitas Bina Sarana Informatika";
     }
 }

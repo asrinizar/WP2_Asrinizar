97653ac22fc64b3a37a353792651b2d744c465c8
nijar8 <asrinizar8@gmail.com>
2024-06-11 10:39:06 +0700

    pertemuan 5 tugas 4

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,13 +2,13 @@
 	<h1><?php echo $judul ?></h1>
 	<h4>Nama</h4>
 	<ul type="disc">
-		<li>Nama Depan : Reza</li>
-		<li>Nama Belakang : Maulana</li>
+		<li>Nama Depan : Asri</li>
+		<li>Nama Belakang : Nizar</li>
 	</ul>
 	<br>
 	<h4>Alamat</h4>
 	<ul type="circle">
-		<li> Jalan Karet</li>
+		<li> Yuka</li>
 	</ul>
 	
 	<h4>Tempat Lahir</h4>
@@ -18,7 +18,7 @@
 
 	<h4>Olah Raga Favorit</h4>
 	<ul type="square">
-		<li>Bulutangkis</li>
+		<li>Memasak</li>
 		<li>Futsal</li>
 	</ul>
 </section>
